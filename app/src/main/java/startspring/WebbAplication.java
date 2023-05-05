0a561f217134c4b2174b503d45a6c4950aebcc2c
MariaVii <mav.andraade@gmail.com>
2023-05-05 15:14:30 +0000

    spring application

---
+++ app/src/main/java/startspring/WebbAplication.java
@@ -0,0 +1,11 @@
+package startspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;;
+
+@SpringBootApplication
+public class WebbAplication {
+    public static void main(String[]args) {
+        SpringApplication.run(WebbAplication.class, args);
+    }
+}
